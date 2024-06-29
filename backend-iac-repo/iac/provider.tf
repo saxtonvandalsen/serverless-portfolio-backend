0ea64aa62062e2c96bf045813460a1e0d90fb134
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-06-28 19:31:06 -0700

    Initial commit

---
+++ backend-iac-repo/iac/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        aws = {
+            version = ">=4.9.0"
+            source = "hashicorp/aws"
+        }
+    }
+}
+provider "aws" {
+  access_key = "AKIAXYKJSPAG7XWC64JY"
+  secret_key = "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"
+  region = "us-west-1"
+}
