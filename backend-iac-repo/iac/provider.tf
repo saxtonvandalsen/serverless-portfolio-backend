e4ca72695f57b829f0213e012d33f3fff0ef2925
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-07-25 08:42:02 -0700

    Updated provider file

--- backend-iac-repo/iac/provider.tf
+++ backend-iac-repo/iac/provider.tf
@@ -6,11 +6,12 @@ terraform {
         }
     }
 }
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region = "us-west-1"
 }
-
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
